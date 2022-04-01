3cb5c6fac4194e57410a9e7f99f74364d290a502
Holger Drewes <Holger.Drewes@gmail.com>
2022-04-01 10:23:22 +0200

    VM -> EVM/VM Refactor: standalone EVM stateManager option and property

--- packages/vm/src/runTx.ts
+++ packages/vm/src/runTx.ts
@@ -395,7 +395,10 @@ async function _runTx(this: VM, opts: RunTxOpts): Promise<RunTxResult> {
     value: value,
     data,
   })
-  const evm = new EVM(this, txContext, block, { common: this._common })
+  const evm = new EVM(this, txContext, block, {
+    common: this._common,
+    stateManager: this.stateManager,
+  })
   if (this.DEBUG) {
     debug(
       `Running tx=0x${
