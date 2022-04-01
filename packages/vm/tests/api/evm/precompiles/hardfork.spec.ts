d2387d43a4fd10bc21265bdfa5c3e314d4034a81
Holger Drewes <Holger.Drewes@gmail.com>
2022-04-01 11:23:06 +0200

    VM -> EVM/VM Refactor: moved runCall()/runCode() to EVM, deleted dedicated files, top-level EVM instantiation in VM (introduces eip-3529 test failures)

--- packages/vm/tests/api/evm/precompiles/hardfork.spec.ts
+++ packages/vm/tests/api/evm/precompiles/hardfork.spec.ts
@@ -22,7 +22,7 @@ tape('Precompiles: hardfork availability', (t) => {
     }
 
     let vm = await VM.create({ common: commonByzantium })
-    let result = await vm.runCall({
+    let result = await vm.evm.runCall({
       caller: Address.zero(),
       gasLimit: BigInt(0xffffffffff),
       to: ECPAIR_Address,
@@ -42,7 +42,7 @@ tape('Precompiles: hardfork availability', (t) => {
     }
 
     vm = await VM.create({ common: commonPetersburg })
-    result = await vm.runCall({
+    result = await vm.evm.runCall({
       caller: Address.zero(),
       gasLimit: BigInt(0xffffffffff),
       to: ECPAIR_Address,
@@ -63,7 +63,7 @@ tape('Precompiles: hardfork availability', (t) => {
 
     vm = await VM.create({ common: commonHomestead })
 
-    result = await vm.runCall({
+    result = await vm.evm.runCall({
       caller: Address.zero(),
       gasLimit: BigInt(0xffffffffff),
       to: ECPAIR_Address,
