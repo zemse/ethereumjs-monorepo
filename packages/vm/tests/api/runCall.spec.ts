37f9f2e65bcdb09287b687807a9bd91f87e041ea
Holger Drewes <Holger.Drewes@gmail.com>
2022-04-01 12:00:20 +0200

    VM -> EVM/VM Refactor: made EVM AsyncEventEmitter, moved beforeMessage, afterMessage, newContract events to EVM

--- packages/vm/tests/api/runCall.spec.ts
+++ packages/vm/tests/api/runCall.spec.ts
@@ -165,7 +165,7 @@ tape('Ensure that precompile activation creates non-empty accounts', async (t) =
   }
 
   const resultNotActivated = await vmNotActivated.evm.runCall(runCallArgs)
-  const resultActivated = await vmActivated.runCall(runCallArgs)
+  const resultActivated = await vmActivated.evm.runCall(runCallArgs)
 
   const diff = resultNotActivated.execResult.gasUsed - resultActivated.execResult.gasUsed
   const expected = BigInt(common.param('gasPrices', 'callNewAccount'))
